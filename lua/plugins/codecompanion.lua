25e256e04f57fb100b023f62d649eb011dea4b9d
artemiycherevko <ArtemiyCherevko@toppanmerrillllc.com>
2024-11-07 00:55:03 +0200

    added codecompanion with github

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,24 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+    "github/copilot.vim",
+    { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+    { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+  },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+      },
+    })
+  end,
+}
