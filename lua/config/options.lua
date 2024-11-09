48e25d27d6043010ed66c2e6f5f038bd96dc4b33
artemiycherevko <ArtemiyCherevko@toppanmerrillllc.com>
2024-11-09 13:22:23 +0200

    using system cliboard, disable copying deleted text to clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- disable copying deleted text to clipboard
+vim.keymap.set({ "n", "v" }, "d", '"_d')
+vim.keymap.set({ "n", "v" }, "D", '"_D')
+vim.keymap.set({ "n", "v" }, "x", '"_x')
