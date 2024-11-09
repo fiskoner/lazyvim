dbf8e8a08ba77948ff0c55b1036b4ad1e6dd3621
artemiycherevko <ArtemiyCherevko@toppanmerrillllc.com>
2024-11-09 13:37:11 +0200

    set fileformat to unix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- set fileformat to unix
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = { "unix", "dos" }
+
 -- use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
